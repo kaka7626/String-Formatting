b629148f4ac9ff85bd3d9189362b40425a44f9d9
kaka7626 <hoang7626@gmail.com>
2023-07-28 23:15:58 +0700

    format string

---
+++ STRING-FORMATTING.py
@@ -0,0 +1,7 @@
+name = 'long'
+age = '24'
+class_type = 'filthy mage'
+experience = '2 years'
+
+print('''My name is %s, i'm %s years old, i'm a fucking %s and i have %s experienced in killing people! '''
+% (name, age, class_type, experience))
